174e36ecc71e570a5eccb43d90cac2e33d99ff97
jubii100 <mohammedjelsiddig@gmail.com>
2025-08-21 11:14:54 +0400

    Update chat/main.py

--- chat/main.py
+++ chat/main.py
@@ -828,7 +828,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=0080,
         reload=True,
         reload_dirs=["."],
         log_level="info"
